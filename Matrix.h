5cae9d2d59bc75aed69a0f4c0418340856b140b0
aayushs98 <aayushat98@gmail.com>
2018-02-22 11:25:58 +0530

    Updated code of Expression Templates

--- Matrix.h
+++ Matrix.h
@@ -7,31 +7,33 @@ class MatrixExp {
 
 	public:
 
-		double operator[](size_t i) const { return static_cast<T const&>(*this)[i]; }
+		auto operator[](size_t i) const { return static_cast<T const&>(*this)[i]; }
 
 		size_t size()               const { return static_cast<T const&>(*this).size(); }
 
-		operator T& () { return static_cast<T&>(*this); }
+		operator T& () 			  { return static_cast<T&>(*this); }
 
-		operator const T& () const { return static_cast<const T&>(*this); }
+		operator const T& () 	    const { return static_cast<const T&>(*this); }
 
 };
 
-class Matrix : public MatrixExp<Matrix> {
-	valarray<double> data;
+template <typename R>
+class Matrix { 
+	valarray<R> data;
 	public:
-	double operator[](size_t i) const { return data[i]; }
-	double &operator[](size_t i)      { return data[i]; }
+
+	R operator[](size_t i) const { return data[i]; }
+	R &operator[](size_t i)      { return data[i]; }
 	size_t size() const               { return data.size(); }
 
 	//data[] contains the matrix elements in the form of 1-D vector
 	//data[0] stores the number of rows
 
-	double operator()(size_t row, size_t col) const{
+	R operator()(size_t row, size_t col) const{
 		return data[data[0]*row + col + 1];
 	}
 
-	double& operator()(size_t row, size_t col){
+	R& operator()(size_t row, size_t col){
 		return data[data[0]*row + col + 1];
 	}
 
@@ -41,19 +43,18 @@ class Matrix : public MatrixExp<Matrix> {
 	//the addition operation occurs only at the destination when in is reached hence, no memory overhead cost
 
 	template <typename T>
-		inline Matrix &operator+=(const MatrixExp<T> &X) {
+		inline Matrix<R> &operator+=(const MatrixExp<T> &X) {
 			this->data[0] = X[0];
-			for(int i = 1; i < X.size(); i++) {
+			for(size_t i = 1; i < X.size(); i++) {
 				this->data[i] = this->data[i] + X[i];
-				cout<<i<<" "<<data[i]<<endl;
 			}
 			return *this;
 		}
 
 	template <typename T>
-		inline Matrix &operator=(const MatrixExp<T> &X) {
+		inline Matrix<R> &operator=(const MatrixExp<T> &X) {
 			this->data[0] = X[0];
-			for(int i = 1; i < X.size(); i++) {
+			for(size_t i = 1; i < X.size(); i++) {
 				this->data[i] = X[i];
 			}
 			return *this;
@@ -65,18 +66,19 @@ class Matrix : public MatrixExp<Matrix> {
 			//(total size - 1) divided by the number of rows gives the number of columns of the matrix
 		
 			assert(((this->data.size()-1)/(this->data[0])) == X[0]);
-			valarray<double> ans((X[0])*((X.size()-1)/X[0]) + 1);
+			valarray<R> ans((X[0])*((X.size()-1)/X[0]) + 1);
+
 			ans[0]=X[0];
 
-			for (int i = 0; i < this->data[0]; i++) {
-				for (int j = 0; j < ((X.size()-1)/(X[0])); j++) {
-					for (int k = 0; k < (this->data.size()-1)/(this->data[0]); k++) {
+			for (size_t i = 0; i < this->data[0]; i++) {
+				for (size_t j = 0; j < ((X.size()-1)/(X[0])); j++) {
+					for (size_t k = 0; k < (this->data.size()-1)/(this->data[0]); k++) {
 	 					ans[X[0]*i + j + 1] += this->data[X[0]*i + k + 1] * X[X[0]*k + j + 1];
 	 				}
 				}
 	 		}
-			for (int i = 0; i < this->data[0]; i++) {
-				for (int j = 0; j < ((X.size()-1)/(X[0])); j++) {
+			for (size_t i = 0; i < this->data[0]; i++) {
+				for (size_t j = 0; j < ((X.size()-1)/(X[0])); j++) {
 					this->data[X[0]*i + j +1] = ans[X[0]*i + j + 1];
 				}
 			}
@@ -101,13 +103,14 @@ class MatrixSum : public MatrixExp<MatrixSum<E1, E2> > {
 		assert(u[0] == v[0] and (u.size()-1) == (v.size() - 1));
 	}
 
-	double operator[](size_t i) const { 
+	
+	auto operator[](size_t i) const { 
 		if(i!=0) {
 			return _u[i] + _v[i];
 		}
 		else return _v[i];
 	}
-	size_t size()               const { 
+	size_t size()	       const { 
 		return _v.size(); 
 	}
 };
@@ -119,16 +122,16 @@ operator+(E1 const& u, E2 const& v) {
 }
 
 template <typename E>
-Matrix const
-operator*(E const& u, E const& v) {
+Matrix<E> const
+operator*(Matrix<E> const& u, Matrix<E> const& v) {
 	
 	assert(u[0] == (v.size()-1)/v[0]);
-	Matrix ans(u[0],((v.size()-1)/v[0]));
-	ans[0]=u[0];
+	Matrix<E> ans(u[0],((v.size()-1)/v[0]));
+	ans[0] = u[0];
 
-	for (int i = 0; i < u[0]; i++) {
-		for (int j = 0; j < (v.size()-1)/v[0]; j++) {
-			for (int k = 0; k < (u.size()-1)/u[0]; k++) {
+	for (size_t i = 0; i < u[0]; i++) {
+		for (size_t j = 0; j < (v.size()-1)/v[0]; j++) {
+			for (size_t k = 0; k < (u.size()-1)/u[0]; k++) {
 				ans(i,j) += u(i,k)*v(k,j);
 			}
 		}
